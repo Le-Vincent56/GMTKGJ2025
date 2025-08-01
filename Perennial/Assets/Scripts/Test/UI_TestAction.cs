5d9bb9975ccfc49f636f9cd8aceb41f85080cc37
William Tallarico <wct9667@rit.edu>
2025-08-01 10:01:39 -0400

    Test UI

--- Perennial/Assets/Scripts/Test/UI_TestAction.cs
+++ Perennial/Assets/Scripts/Test/UI_TestAction.cs
@@ -1,17 +1,23 @@
+using Perennial.Actions.Commands;
+using Perennial.Core.Architecture.Event_Bus;
+using Perennial.Core.Architecture.Event_Bus.Events;
 using UnityEngine;
 
 namespace Perennial.Test
 {
     public class UI_TestAction : MonoBehaviour
     {
-        private UnityEngine.UI.Button _buttonPassTurn;
+        private UnityEngine.UI.Button _button;
 
         void Start()
         {
-            _buttonPassTurn = GetComponent<UnityEngine.UI.Button>();
-            _buttonPassTurn.onClick.AddListener(() =>
+            _button = GetComponent<UnityEngine.UI.Button>();
+            _button.onClick.AddListener(() =>
             {
-                EventBus<EndTurn>.Raise(new EndTurn());
+                EventBus<PerformCommand>.Raise(new PerformCommand()
+                {
+                    Command =  BaseCommand.Create<HarvestCommand>()
+                });
             }); 
         }
     }
