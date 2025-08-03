3d70ea0a0354d3879a9901b134ef5a7c44910827
William Tallarico <wct9667@rit.edu>
2025-08-03 11:12:09 -0400

    prefab updates

--- Perennial/Assets/Scripts/Actions/UI/WinPopup.cs
+++ Perennial/Assets/Scripts/Actions/UI/WinPopup.cs
@@ -2,6 +2,7 @@ using System;
 using Perennial.Core.Architecture.Event_Bus;
 using Perennial.Core.Architecture.Event_Bus.Events;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Perennial
 {
@@ -9,12 +10,43 @@ namespace Perennial
     {
         [SerializeField] private UnityEngine.UI.Button replayButton;
         [SerializeField] private UnityEngine.UI.Button quitButton;
+        [SerializeField] private Canvas winCanvas;
 
 
         private EventBinding<WinGameEvent> winGameEvent;
+
+        private void Start()
+        {
+            winCanvas = GetComponent<Canvas>();
+            winCanvas.enabled = false;
+            replayButton.onClick.AddListener(Replay);
+            quitButton.onClick.AddListener(LoadMainMenu);
+        }
+
         private void OnEnable()
         {
-           
+            winGameEvent = new EventBinding<WinGameEvent>(WinGame);
+            EventBus<WinGameEvent>.Register(winGameEvent);
+        }
+
+        private void OnDisable()
+        {
+            EventBus<WinGameEvent>.Deregister(winGameEvent);
+        }
+
+        private void WinGame()
+        {
+            winCanvas.enabled = true;
+        }
+
+        private void LoadMainMenu()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        private void Replay()
+        {
+            SceneManager.LoadScene(0);
         }
     }
 }
