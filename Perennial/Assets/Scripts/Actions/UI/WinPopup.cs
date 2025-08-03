c2f0f32aaba59a07e0a7bf8570deafb8367b7691
Frank Alfano <q24xboss@gmail.com>
2025-08-03 12:01:31 -0400

    Added lose event function

--- Perennial/Assets/Scripts/Actions/UI/WinPopup.cs
+++ Perennial/Assets/Scripts/Actions/UI/WinPopup.cs
@@ -1,19 +1,24 @@
 using System;
 using Perennial.Core.Architecture.Event_Bus;
 using Perennial.Core.Architecture.Event_Bus.Events;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Perennial
 {
     public class WinPopup : MonoBehaviour
     {
-        [SerializeField] private UnityEngine.UI.Button replayButton;
-        [SerializeField] private UnityEngine.UI.Button quitButton;
+        [SerializeField] private TextMeshProUGUI titleText;
+        [SerializeField] private Image titleImage;
+		[SerializeField] private Button replayButton;
+        [SerializeField] private Button quitButton;
         [SerializeField] private Canvas winCanvas;
 
 
         private EventBinding<WinGameEvent> winGameEvent;
+        private EventBinding<LoseGameEvent> loseGameEvent;
 
         private void Start()
         {
@@ -26,18 +31,31 @@ namespace Perennial
         private void OnEnable()
         {
             winGameEvent = new EventBinding<WinGameEvent>(WinGame);
-            EventBus<WinGameEvent>.Register(winGameEvent);
+            loseGameEvent = new EventBinding<LoseGameEvent>(LoseGame);
+
+			EventBus<WinGameEvent>.Register(winGameEvent);
+            EventBus<LoseGameEvent>.Register(loseGameEvent);
         }
 
         private void OnDisable()
         {
             EventBus<WinGameEvent>.Deregister(winGameEvent);
-        }
+			EventBus<LoseGameEvent>.Deregister(loseGameEvent);
+		}
 
         private void WinGame()
         {
             winCanvas.enabled = true;
-        }
+            titleText.text = "you won";
+            titleImage.color = new Color(158 / 255f, 198 / 255f, 153 / 255f);
+		}
+
+        private void LoseGame()
+        {
+            winCanvas.enabled = true;
+			titleText.text = "you lose";
+			titleImage.color = new Color(202 / 255f, 73 / 255f, 72 / 255f);
+		}
 
         private void LoadMainMenu()
         {
