46ae8e137410dcf0aa6f2a34c91add8a7f3ec2aa
William Tallarico <wct9667@rit.edu>
2025-08-03 10:56:14 -0400

    food logic

---
+++ Perennial/Assets/Scripts/Actions/UI/WinPopup.cs
@@ -0,0 +1,20 @@
+using System;
+using Perennial.Core.Architecture.Event_Bus;
+using Perennial.Core.Architecture.Event_Bus.Events;
+using UnityEngine;
+
+namespace Perennial
+{
+    public class WinPopup : MonoBehaviour
+    {
+        [SerializeField] private UnityEngine.UI.Button replayButton;
+        [SerializeField] private UnityEngine.UI.Button quitButton;
+
+
+        private EventBinding<WinGameEvent> winGameEvent;
+        private void OnEnable()
+        {
+            win
+        }
+    }
+}
